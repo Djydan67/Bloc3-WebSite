41f5bd86b857d3f7722f7ab6cd2cca58007179d9
BetterCallSalar <salar.eini.cr@gmail.com>
2024-05-16 11:22:23 +0200

    Added Mother Controller - The header and footer are commented out

---
+++ Controller/mother_controller.php
@@ -0,0 +1,20 @@
+<?php
+    // Salar
+    // Controller parent
+    
+    class Ctrl{
+
+        // array for user's data
+        protected array $_arrData = array();
+
+        protected function prepare(string $strArgument){
+            // Converting the array of the client's data to Key/Value
+            foreach ($this->_arrData as $key=>$value){
+                $$key = $value;
+            }
+
+            // include("views/_partial/header.php");
+            include("views/".$strArgument.".php");
+            // include("views/_partial/footer.php");
+        }
+    }
