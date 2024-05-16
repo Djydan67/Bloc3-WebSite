eda180e220ab21767157251f13138932d19af2fa
Djydan67 <renaud.siegel@viacesi.fr>
2024-05-16 14:53:39 +0200

    a

---
+++ View/stuff.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Assets/Style/style.css">
+    <link rel="stylesheet" href="Assets/Style/Bootstrap/bootstrap.css"/>
+    <title>Equipement</title>
+</head>
+<body class="fond">
+    <header id="entete">
+        <h1 id="title">Equipement</h1>
+        <nav>
+            <div id="corps">
+                <img src="Assets/Images/logo-dofus.png" alt="Logo" id="logo"/>
+                <div id="filtre">
+                    <p>
+                        <img src="Assets/Images/logo-amulette.png" alt="logo-amulette" id="Amulette"/>
+                        <!-- <label>Tête</label>
+                        <input type="checkbox" id="tete"> -->
+                    </p>
+                    <p>
+                        <img src="Assets/Images/logo-ceinture.png" alt="logo-amulette" id="Ceinture"/>
+                        <!-- <label>Torse</label>
+                        <input type="checkbox" id="torse">  -->
+                    </p>
+                    <p>
+                        <img src="Assets/Images/logo-dofus_(2).png" alt="logo-amulette" id="Dofus"/>
+                        <!-- <label>Bras</label>
+                        <input type="checkbox" id="bras"> -->
+                    </p>
+                    <p>
+                        <img src="Assets/Images/logo-botte.png" alt="logo-amulette" id="Bottes"/>
+                        <!-- <label>Botte</label>
+                        <input type="checkbox" id="Botte">   -->
+                    </p>                    
+                    <p>
+                        <img src="Assets/Images/logo-cape.png" alt="logo-amulette" id="Cape"/>
+                        <!-- <label>Botte</label>
+                        <input type="checkbox" id="Botte">   -->
+                    </p>                    
+                    <p>
+                        <img src="Assets/Images/logo-chapeau.png" alt="logo-amulette" id="Chapeau"/>
+                        <!-- <label>Botte</label>
+                        <input type="checkbox" id="Botte">   -->
+                    </p>
+                        <img src="Assets/Images/logo-anneaux.png" alt="logo-amulette" id="Anneau"/>
+                        <!-- <label>Botte</label>
+                        <input type="checkbox" id="Botte">   -->
+                    </p>
+                        <img src="Assets/Images/logo-trophee.png" alt="logo-amulette" id="Trophee"/>
+                        <!-- <label>Botte</label>
+                        <input type="checkbox" id="Botte">   -->
+                    </p>
+                        <img src="Assets/Images/logo-bouclier.png" alt="logo-amulette" id="Bouclier"/>
+                        <!-- <label>Botte</label>
+                        <input type="checkbox" id="Botte">   -->
+                    </p>
+                    <p>
+                        <img src="Assets/Images/logo-arme.png" alt="logo-amulette" id="Armes"/>
+                        <!-- <label>Arme</label>
+                        <input type="checkbox" id="arme"> -->
+                    </p>
+                </div>            
+            </div>
+        </nav>
+         
+    </header>
+    <main class="fond">
+            <div class="row" id="listing"></div>
+    </main>
+    <footer>
+
+    </footer>
+    <script src="Assets/JS/stuff.js"></script>
+</body>
+</html>
