dc9d6f085cc93edc8b45f2a0db5dff9f7d32b828
ilian <ilianmahzem@orange.fr>
2024-06-11 11:35:26 +0200

    Création de index.tsx

--- Mobile/dofusMobile/app/(tabs)/index.tsx
+++ Mobile/dofusMobile/app/(tabs)/index.tsx
@@ -1,49 +1,57 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from "react";
+import { Image, StyleSheet, useWindowDimensions } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Link } from "expo-router";
+
+const Carousel = [
+  {
+    id: "image_del",
+  },
+  {
+    id: "image_equip",
+  },
+  {
+    id: "image_forum",
+  },
+];
 
 export default function HomeScreen() {
+  const { width } = useWindowDimensions();
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/dofushelp_banner.jpg")}
           style={styles.reactLogo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Bienvenue sur DofusHelp !</ThemedText>
         <HelloWave />
+        <Link
+          href={{
+            pathname: "/profil",
+            // params: { userId: user.id },
+          }}
+        >
+          <Image
+            source={require("@/assets/images/profil.png")}
+            style={styles.profilLogo}
+          />
+        </Link>
       </ThemedView>
+      <ThemedView></ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+        <ThemedText type="subtitle">
+          Sur notre site, vous pourrez tester tout les équipements du jeux avec
+          votre personnage ! Essayer vos combinaisons pour optimiser votre perso
+          au maximum !
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
@@ -52,8 +60,8 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -61,10 +69,23 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
+    position: "absolute",
+    top: 0,
     left: 0,
-    position: 'absolute',
+    width: "100%",
+    height: "100%",
+    zIndex: -1 /* Pour placer l'image derrière le texte */,
+  },
+  profilLogo: {
+    width: 40, // Taille du conteneur pour l'icône de profil
+    height: 40, // Taille du conteneur pour l'icône de profil
+    borderRadius: 20, // Pour rendre l'icône ronde (la moitié de la largeur/hauteur)
+    overflow: "hidden", // Pour s'assurer que l'image ne dépasse pas du conteneur
+    backgroundColor: "#fff", // Optionnel : fond blanc pour l'icône
+    shadowColor: "#000", // Optionnel : ombre pour l'icône
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 3, // Nécessaire pour les ombres sur Android
   },
 });
