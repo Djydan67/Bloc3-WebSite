6972bf1f4aebc61a6c4b571481e4e1abfbc4dbab
Djydan67 <renaud.siegel@viacesi.fr>
2024-06-15 15:21:55 +0200

    index.php envoie sur index_view.php, ajout de index_controller.php, ajout de nav.php, correction des soucis sur header.php, correction des routes sur certains fichiers, correction des problèmes d'affichage sur la bibliothèque

--- Controller/mother_controller.php
+++ Controller/mother_controller.php
@@ -8,7 +8,7 @@
         // array for user's data
         protected array $_arrData = array();
 
-        protected function prepare(string $strArgument){
+        protected function display(string $strArgument){
             // Converting the array of the client's data to Key/Value
             foreach ($this->_arrData as $key=>$value){
                 $$key = $value;
--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
-include("Controller/mother_controller.php");
-$strCtrl    = $_GET['ctrl'] ?? "stuff";
-$strAction  = $_GET['action'] ?? "equipements";
-require("Controller/" . $strCtrl . "_controller.php");
-$strClassName   = ucfirst($strCtrl) . "_Ctrl";
-$objCtrl        = new $strClassName;
-$objCtrl->$strAction();
+
+    session_start();
+
+    $strCtrl    = $_GET['ctrl'] ?? "index";
+    $strAction  = $_GET['action'] ?? "index";
+
+    include("Controller/mother_controller.php");
+
+    //Fait un test de verificaiton
+    if(file_exists("Controller/".$strCtrl."_controller.php")){
+        require("Controller/" . $strCtrl . "_controller.php");
+        $strClassName   = ucfirst($strCtrl) . "_Ctrl";
+        $objCtrl        = new $strClassName;
+        $objCtrl->$strAction();
+    }else{
+        header("Location:index.php?ctrl=error&action=error_404");
+    }
