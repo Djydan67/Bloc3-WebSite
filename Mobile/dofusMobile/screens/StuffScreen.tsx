122f806720ad241dea550d8231cb5a4d347ae9e4
Djydan67 <renaud.siegel@viacesi.fr>
2024-07-03 15:30:09 +0200

    mise en commentaire d\une ligne qui provoquait une erreur sur l\application, dans le fichier index.tsx

--- Mobile/dofusMobile/screens/StuffScreen.tsx
+++ Mobile/dofusMobile/screens/StuffScreen.tsx
@@ -3,47 +3,45 @@ import { useLocalSearchParams, useNavigation } from "expo-router";
 import { Pressable, Text, Touchable, View, FlatList } from "react-native";
 
 export function StuffScreen() {
-    const {piecesId} = useLocalSearchParams();
-    const stuff = useStuff(piecesId as string);
-    const navigation = useNavigation();
-    //console.log(stuff.stuff_name);
-    // navigation.setOptions({
-    //   title:  stuffName || "",
-    // });
+  const { piecesId } = useLocalSearchParams();
+  const stuff = useStuff(piecesId as string);
+  const navigation = useNavigation();
+  //console.log(stuff.stuff_name);
+  // navigation.setOptions({
+  //   title:  stuffName || "",
+  // });
 
-    if (stuff) {
-      return (
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-            <FlatList
-              data={stuff}
-              renderItem={({item}) =>               
-                <Text>{item.stuff_name}</Text>
-              }
-            />
-        </View>
-      );
-    }
-    //console.log(userId)
+  if (stuff) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <FlatList
+          data={stuff}
+          renderItem={({ item }) => <Text>{item.stuff_name}</Text>}
+        />
+      </View>
+    );
+  }
+  //console.log(piecesId);
 
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center",
-//       }}
-//     >
-//         <Text style={{
-//           fontSize:50,
-//         }}>{counter}</Text>
-//         <Text>User {userId}</Text>
-//         <Text>{user.firstName} {user.lastName}</Text>
-//     </View>
+  //   return (
+  //     <View
+  //       style={{
+  //         flex: 1,
+  //         justifyContent: "center",
+  //         alignItems: "center",
+  //       }}
+  //     >
+  //         <Text style={{
+  //           fontSize:50,
+  //         }}>{counter}</Text>
+  //         <Text>User {userId}</Text>
+  //         <Text>{user.firstName} {user.lastName}</Text>
+  //     </View>
   //);
 }
