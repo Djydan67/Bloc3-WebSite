fb2e87c76b101a484c6de28daaf4c94dbffd320d
Nidrae <cazala.adrien1999@gmail.com>
2024-08-17 14:22:55 +0200

    commit des changement de la page de profil

--- Mobile/dofusMobile/hooks/useLogin.ts
+++ Mobile/dofusMobile/hooks/useLogin.ts
@@ -7,7 +7,7 @@ export const useLogin = () => {
   const login = async (email: string, password: string) => {
     try {
       const response = await fetch(
-        "http://192.168.56.1:8082/Bloc3-WebSite/index.php/?ctrl=User&action=loginMobile",
+        "http://localhost:8888/blog/dofusweb/Bloc3-WebSite/index.php/?ctrl=User&action=loginmobile",
         {
           method: "POST",
           headers: {
